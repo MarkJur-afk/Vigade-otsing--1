6cf4422bcac5c3eed4ccfe55c7add3233f7d508b
MarkJur-afk <jurgenmarkk@gmail.com>
2025-02-17 13:49:09 +0200

    Update VigadeOtsing2.py

--- VigadeOtsing2/VigadeOtsing2.py
+++ VigadeOtsing2/VigadeOtsing2.py
@@ -1,17 +1,17 @@
 import math
 
 try:
-        a = float(input('Enter the side length of the square => '))
-        if a > 0:
-            S = a**2
-            print("Square area:", S)
-            P = 4 * a
-            print("Square perimeter:", P)
-            di = a * math.sqrt(2)
-            print("Square diagonal:", round(di, 2))
-        else:
-            print("peab olla ainult positivsest arved")
-except Exception as e:
+    a = float(input('Enter the side length of the square => '))
+    if a > 0:
+        S = a**2
+        print("Square area:", S)
+        P = 4 * a
+        print("Square perimeter:", P)
+        di = a * math.sqrt(2)
+        print("Square diagonal:", round(di, 2))
+    else:
+        print("The side length must be a positive number.")
+except ValueError:
     print("Please enter a valid number.")
 
 print()
@@ -19,7 +19,7 @@ print()
 try:
     b = float(input("Enter the first side length of the rectangle => "))
     c = float(input("Enter the second side length of the rectangle => "))
-    if a > 0 and b > 0:
+    if b > 0 and c > 0:
         S = b * c
         print("Rectangle area:", S)
         P = 2 * (b + c)
@@ -27,23 +27,22 @@ try:
         di = math.sqrt(b**2 + c**2)
         print("Rectangle diagonal:", round(di, 2))
     else:
-        print("Please enter a valid number.")
-except Exception as e:
-    print("Please enter a valid number.")
+        print("Both side lengths must be positive numbers.")
+except ValueError:
+    print("Please enter valid numbers.")
 
 print()
 
 try:
+    r = float(input('Enter the radius of the circle => '))
     if r > 0:
-        r = float(input('Enter the radius of the circle => '))
-        if r > 0:
-            d = 2 * r
-            print("Circle diameter:", d)
-            S = math.pi * r**2
-            print("Circle area:", round(S, 2))
-            C = 2 * math.pi * r
-            print("Circumference:", round(C, 2))
-        else:
-            print("Please enter a valid number.")
-except Exception as e:
+        d = 2 * r
+        print("Circle diameter:", d)
+        S = math.pi * r**2
+        print("Circle area:", round(S, 2))
+        C = 2 * math.pi * r
+        print("Circumference:", round(C, 2))
+    else:
+        print("The radius must be a positive number.")
+except ValueError:
     print("Please enter a valid number.")
